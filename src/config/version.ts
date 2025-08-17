7fef99476bc5f8dc10b2b328d37459f5f3a1dcb9
WonukCha <cwu5529@gmail.com>
2025-08-17 19:47:13 +0900

    Revert "Optimize images - Reduce total size from 215MB to 2.5MB (98.8% reduction)"
    This reverts commit 6febc5ff0eeec722739c2205b6a8b37fcdbe4487.

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-08-17T10:43:32.140Z
+// Build Time: 2025-08-17T10:39:30.850Z
 export const APP_VERSION = {
-  version: '1755427412140',
-  buildTime: '2025-08-17T10:43:32.140Z',
-  hash: 'ef1dijkmxra'
+  version: '1755427170850',
+  buildTime: '2025-08-17T10:39:30.850Z',
+  hash: '008dbsyjx5lr'
 };
 
 export default APP_VERSION;
